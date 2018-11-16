355814af04c83c6dc87f4b1ef60b5c2118306d13
dan <dlhede1509@gmail.com>
2018-11-15 19:42:34 -0600

    created destroy path for stocks

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -2,6 +2,10 @@ class Stock < ApplicationRecord
   has_many :user_stocks 
   has_many :users, through: :user_stocks 
 
+  def self.find_by_ticker(ticker_symbol)
+    
+  end
+
   def self.new_from_lookup(ticker_symbol)
     begin    
       looked_up_stock = StockQuote::Stock.quote(ticker_symbol)
